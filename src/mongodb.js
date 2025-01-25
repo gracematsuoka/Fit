bb955e53f9556b4dca26b3fd39d0727d9d7f12d1
Grace Matsuoka <gracelmatsuoka@gmail.com>
2025-01-25 13:22:54 -0500

    fixed password typo

--- src/mongodb.js
+++ src/mongodb.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const mongoURI = process.env.MONGO_URI;
-console.log("mongo uri: " + mongoURI)
 
 mongoose.connect(mongoURI)
 .then(()=>{
